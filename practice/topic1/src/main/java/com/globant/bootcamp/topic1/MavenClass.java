558f802d75dfef0cb04a225fc5753f6e60fe4cc2
Alvaro Pedraza <aepedraza3117@gmail.com>
2016-06-15 13:01:07 -0300

    Topic 1: Excercise 1

---
+++ practice/topic1/src/main/java/com/globant/bootcamp/topic1/MavenClass.java
@@ -0,0 +1,8 @@
+package com.globant.bootcamp.topic1;
+
+public class MavenClass {
+
+	public void doSomething() {
+		System.out.println("Doing something...");
+	}
+}
---
+++ practice/topic1/src/main/java/com/globant/bootcamp/topic1/MavenMain.java
@@ -0,0 +1,10 @@
+package com.globant.bootcamp.topic1;
+
+public class MavenMain {
+
+	public static void main(String[] args) {
+		MavenClass mavenClass = new MavenClass();
+		mavenClass.doSomething();
+	}
+
+}
